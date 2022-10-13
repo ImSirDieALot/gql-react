80b2e6a0353bbe290c4c2d7cb92f1a9b34d603b2
Sai Krishna Paluru <saikrishna.paluru1@t-mobile.com>
2022-10-13 09:50:22 -0400

    React-GraphQL Demo Project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// helps us inform Apollo about which URL to use when fetching data.
+import { ApolloClient } from '@apollo/client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+// ApolloProvider expects to receive the client object to be able to connect Apollo to React.
+import { ApolloProvider } from '@apollo/react-hooks';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
